6a215346fd44925bf7dec95d9b9a5ee647d5b379
shou-1202 <shouryagarg1202@gmail.com>
2026-08-16 11:52:37 +0530

    Add files via upload

---
+++ 123. Best Time to Buy and Sell Stock III/Solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>>after(2, vector<int>(3, 0)), curr(2, vector<int>(3, 0));
+        for(int i = n-1; i>=0; i--){
+            for(int buy = 0; buy <=1; buy++){
+                for(int count = 1; count<=2; count++){
+                    long profit = -1e9, take = -1e9, notTake = -1e9;
+                    if(buy){
+                       take = -prices[i] + after[0][count];
+                       notTake = after[1][count];
+                       profit = max(take, notTake);
+                    }
+                    else{
+                       take = prices[i]+after[1][count-1];
+                       notTake = after[0][count];
+                       profit = max(take, notTake);
+                    }
+                    curr[buy][count] = profit;
+                }
+            }
+            after = curr;
+        }
+        return after[1][2];
+    }
+};
