c07c4d16179e2eaf75997e360c70f7cc983b321e
shou-1202 <shouryagarg1202@gmail.com>
2026-08-17 09:55:11 +0530

    Add files via upload

---
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee/Solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        int curr0 = 0, curr1 = 0, next0 = 0, next1 = 0;
+        for(int i = n-1; i>=0; i--){
+            for(int buy = 0; buy<=1; buy++){
+                long profit = -1e9;
+                if(buy){
+                   profit = max(-prices[i]-fee+next0, next1);
+                   curr1 = profit;
+                }
+                else{
+                   profit = max(prices[i]+next1, next0);
+                   curr0 = profit;
+                }
+            }
+            next0 = curr0;
+            next1 = curr1;
+        }
+        return next1;
+    }
+};
